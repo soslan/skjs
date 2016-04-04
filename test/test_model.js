ea79c7719f725a4392d5b7e7b9506f8d045fed3d
Soslan Khubulov <soslanx@gmail.com>
2016-04-04 12:05:30 +0300

    Add more tests for Model

--- test/test_model.js
+++ test/test_model.js
@@ -3,6 +3,16 @@ suite("Model tests", function(){
     m = sk.model();
     assert.equal(m.get(), undefined);
   });
+
+  suite("Initialization options", function(){
+    test("value initialization", function(assert){
+      m = sk.model({
+        value: 1,
+      });
+      assert.equal(m.value, 1);
+    });
+  });
+
   test("setting and getting value", function(assert){
     m = sk.model();
     m.set(1);
@@ -30,4 +40,51 @@ suite("Model tests", function(){
     });
     m.value = 4;
   }, 1);
+  test("advanced set()", function(assert){
+    m1 = sk.model();
+    m2 = sk.model({
+      value: 2,
+    });
+    m3 = sk.model({
+      value: 3,
+    });
+    m1.set(m2, m3, function(val2, val3){
+      return val2 + val3;
+    });
+    assert.equal(m1.value, 5);
+    m2.set(7);
+    assert.equal(m1.value, 10);
+  });
+  test("binding two models", function(assert){
+    m1 = sk.model({
+      value: 1,
+    });
+    m2 = sk.model({
+      value: 2,
+    });
+    m1.bind(m2, function(m2Val){
+      return m2Val + 10;
+    }, function(m1Val){
+      return m1Val - 10;
+    });
+    assert.equal(m1.value, 12);
+    m1.set(10);
+    assert.equal(m2.get(), 0);
+    m2.set(20);
+    assert.equal(m1.get(), 30);
+  });
+  test("infinite loop prevention", function(assert){
+    m1 = sk.model();
+    m2 = sk.model({
+      value: 1,
+    });
+
+    m1.bind(m2, function(val2){
+      return val2 + 1;
+    }, function(val1){
+      return val1 + 1;
+    });
+    assert.equal(m1.value, 2);
+    assert.equal(m2.value, 3);
+  });
 });
