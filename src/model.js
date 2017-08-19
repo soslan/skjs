11ea06bf6953a8554854ee3ae976dc0d8e4b2b89
Soslan Khubulov <soslanx@gmail.com>
2017-08-19 21:30:28 +0300

    Rewrite Model

--- src/model.js
+++ src/model.js
@@ -4,132 +4,47 @@ function model(args){
 
 function Model(args){
   this.valueCore;
-  this.listeners = [];
-  this.filters = [];
+  this.listeners;
+  this.filters;
 
   this.init(args);
 }
 
 sk.Model = Model;
 
-Model.prototype.set = function(arg1, arg2, arg3){
-  var self = this;
-  var values = [];
-  var customUpstream, filter, val;
-  if(arguments.length >= 3){
-    for (var i=0; i<arguments.length-2; i++){
-      values.push(arguments[i]);
-    }
-    if(typeof arguments[arguments.length-2] === "function"){
-      filter = arguments[arguments.length-2];
-      customUpstream = arguments[arguments.length-1];
-    }
-    else{
-      values.push(arguments[arguments.length-2]);
-      filter = arguments[arguments.length-1];
-    }
-  }
-  else{
-    values.push(arg1);
-    if(typeof arg2 === "function"){
-      filter = arg2;
-    }
-    else {
-      customUpstream = arg2;
-    }
-  }
-  values.forEach(function(value){
-    if(value instanceof Model){
-      value.listen(function(val, upstream){
-        upstream = upstream.concat(customUpstream);
-        if(upstream && upstream.indexOf(self) !== -1){
-          return;
-        }
-        if(typeof filter === "function"){
-          val = filter.apply(this, values.map(function(value){
-            if(value instanceof Model){
-              return value.get();
-            }
-            else{
-              return value;
-            }
-          }));
-        }
-        self.setValue(val, upstream);
-      });
-    }
-  });
-  if(typeof filter === "function"){
-    val = filter.apply(this, values.map(function(value){
-      if(value instanceof Model){
-        return value.get();
-      }
-      else{
-        return value;
-      }
-    }));
-  }
-  else{
-    if(arg1 instanceof Model){
-      val = arg1.get();
-    }
-    else{
-      val = arg1;
-    }
-  }
-  self.setValue(val, customUpstream);
-}
+Model.prototype.isSkModel = true;
 
-Model.prototype.setValue = function(val, upstream){
-  var self = this, cancel = false;
+Model.prototype.set = function(val, upstream){
+  var self = this, i, cancel = false;
   if(upstream == null){
-    upstream = [self];
+    upstream = [];
   }
-  for (var i in this.filters){
-    val = this.filters[i](val, {
-      cancel: function(){cancel=true;},
-      upstream: upstream,
-    });
-    if(cancel){
+  else if(upstream.indexOf(self) !== -1){
+    return;
+  }
+  var event = {
+    cancel: false,
+    upstream: upstream || [],
+    target: self,
+    value: val,
+  }
+  this.valueCore = event.value;
+  event.upstream.push(self);
+  for (i in this.listeners){
+    this.listeners[i](event);
+    if(event.cancel){
       return;
     }
   }
-  if(this.valueCore === val){
-    return;
-  }
-  this.valueCore = val;
-  upstream.push(self);
-  this.listeners.forEach(function(handler){
-    handler(self.value, upstream);
-  });
 }
 
 Model.prototype.get = function(){
   return this.valueCore;
 }
 
-Model.prototype.bind = function(arg1, upstreamFilter, downstreamFilter){
-  if(arg1 instanceof Model){
-    this.set(arg1, upstreamFilter);
-    //arg1.set(this, downstreamFilter);
-    this.listen(function(val, upstream){
-      if(upstream && upstream.indexOf(arg1) !== -1){
-        return;
-      }
-      if(typeof downstreamFilter === "function"){
-        val = downstreamFilter(val);
-      }
-      arg1.setValue(val, upstream);
-    });
-  }
-  else{
-    this.set(arg1, upstreamFilter);
-  }
-}
-Model.prototype.sync = Model.prototype.bind;
-
 Model.prototype.init = function(args){
   args = args || {};
+  this.value = args.initial;
   if(args.value !== undefined){
     this.value = args.value;
   }
@@ -145,14 +60,22 @@ Model.prototype.init = function(args){
 }
 
 Model.prototype.listen = function(handler){
+  if(!this.listeners){
+    this.listeners = [];
+  }
   if(typeof handler === 'function'){
     this.listeners.push(handler);
-    handler(this.get(), [this]);
+    handler({
+      value: this.value,
+    }, [this]);
   }
 };
 Model.prototype.onvalue = Model.prototype.listen;
 
 Model.prototype.onchange = function(handler){
+  if(!this.listeners){
+    this.listeners = [];
+  }
   if(typeof handler === 'function'){
     this.listeners.push(handler);
   }
@@ -176,9 +99,9 @@ Object.defineProperty(Model.prototype, 'value', {
 
 Model.prototype.true = function(listener){
   if(typeof listener === "function"){
-    this.listen(function(val){
-      if(Boolean(val)){
-        listener(Boolean(val));
+    this.listen(function(e){
+      if(Boolean(e.val)){
+        listener(Boolean(e.val));
       }
     });
   }
@@ -189,9 +112,9 @@ Model.prototype.true = function(listener){
 
 Model.prototype.false = function(listener){
   if(typeof listener === "function"){
-    this.listen(function(val){
-      if(!Boolean(val)){
-        listener(Boolean(val));
+    this.listen(function(e){
+      if(!Boolean(e.val)){
+        listener(Boolean(e.val));
       }
     });
   }
@@ -200,7 +123,7 @@ Model.prototype.false = function(listener){
   }
 }
 
-Model.prototype.property = function(prop, val){
+Model.prototype.prop = function(prop, val){
   var self = this;
   this.value[prop] = val;
   this.set(this.value);
@@ -208,93 +131,96 @@ Model.prototype.property = function(prop, val){
     handler(self.value, [self]);
   });
 };
+Model.prototype.property = Model.prototype.prop;
 
-Model.prototype.propertyModel = function(prop){
-  var model = sk.model();
-  var self = this;
-  model.bind(this, function(parent){
-    return parent[prop];
-  }, function(val){
-    self.property(prop, val);
-    return self.value;
-  });
-  return model;
-}
+// Model.prototype.propertyModel = function(prop){
+//   var model = sk.model();
+//   var self = this;
+//   model.bind(this, function(parent){
+//     return parent[prop];
+//   }, function(val){
+//     self.property(prop, val);
+//     return self.value;
+//   });
+//   return model;
+// }
 
-Model.prototype.switchClasses = function(elem, classes){
-  if(typeof classes === "string"){
-    classes = {
-      true: classes,
-    }
-  }
-  if(elem instanceof Element){
-    this.listen(function(val){
-      if(Boolean(val)){
-        if(classes.true != null){
-          elem.classList.add(String(classes.true));
-        }
-        if(classes.false != null){
-          elem.classList.remove(String(classes.false));
-        }
-      }
-      else{
-        if(classes.true != null){
-          elem.classList.remove(String(classes.true));
-        }
-        if(classes.false != null){
-          elem.classList.add(String(classes.false));
-        }
-      }
-    });
-  }
-};
-Model.prototype.switchClass = Model.prototype.switchClasses;
+// Model.prototype.switchClasses = function(elem, classes){
+//   if(typeof classes === "string"){
+//     classes = {
+//       true: classes,
+//     }
+//   }
+//   if(elem instanceof Element){
+//     this.listen(function(e){
+//       var val = e.value;
+//       if(Boolean(val)){
+//         if(classes.true != null){
+//           elem.classList.add(String(classes.true));
+//         }
+//         if(classes.false != null){
+//           elem.classList.remove(String(classes.false));
+//         }
+//       }
+//       else{
+//         if(classes.true != null){
+//           elem.classList.remove(String(classes.true));
+//         }
+//         if(classes.false != null){
+//           elem.classList.add(String(classes.false));
+//         }
+//       }
+//     });
+//   }
+// };
+// Model.prototype.switchClass = Model.prototype.switchClasses;
 
-Model.prototype.setAsClass = function(elem){
-  if(elem instanceof Element){
-    this.listen(function(val){
-      elem.classList.add(String(this.value));
-    });
-  }
-}
+// Model.prototype.setAsClass = function(elem){
+//   if(elem instanceof Element){
+//     this.listen(function(val){
+//       elem.classList.add(String(this.value));
+//     });
+//   }
+// }
 
-Model.prototype.syncWithStorage = function(item, storage){
-  var self = this;
-  storage = storage || localStorage;
-  this.value = storage.getItem(item);
-  this.listen(function(val){
-    storage.setItem(item, String(val));
-  });
-  window.addEventListener('storage', function(e){
-    console.log("STORAGE EVENT", e);
-    self.value = e.newValue;
-  });
-}
+// Model.prototype.syncWithStorage = function(item, storage){
+//   var self = this;
+//   storage = storage || localStorage;
+//   this.value = storage.getItem(item);
+//   this.listen(function(val){
+//     storage.setItem(item, String(val));
+//   });
+//   window.addEventListener('storage', function(e){
+//     console.log("STORAGE EVENT", e);
+//     self.value = e.newValue;
+//   });
+// }
 
-// Experimental
-Model.prototype.syncWithScrollTopP = function(elem){
-  var self = this;
-  if(elem instanceof Element){
-    this.value = elem.scrollTop / (elem.scrollHeight - elem.clientHeight);
+// // Experimental
+// Model.prototype.syncWithScrollTopP = function(elem){
+//   var self = this;
+//   if(elem instanceof Element){
+//     this.value = elem.scrollTop / (elem.scrollHeight - elem.clientHeight);
 
-    elem.addEventListener('scroll', function(e){
-      self.value = elem.scrollTop / (elem.scrollHeight - elem.clientHeight);
-    });
-  }
-  else if(elem instanceof Document || elem === window){
-    elem = document;
-    var body = elem.body;
-    this.value = body.scrollTop / (body.scrollHeight - window.innerHeight);
+//     elem.addEventListener('scroll', function(e){
+//       self.value = elem.scrollTop / (elem.scrollHeight - elem.clientHeight);
+//     });
+//   }
+//   else if(elem instanceof Document || elem === window){
+//     elem = document;
+//     var body = elem.body;
+//     this.value = body.scrollTop / (body.scrollHeight - window.innerHeight);
 
-    elem.addEventListener('scroll', function(e){
-      self.value = body.scrollTop / (body.scrollHeight - window.innerHeight);
-    });
-  }
-}
+//     elem.addEventListener('scroll', function(e){
+//       self.value = body.scrollTop / (body.scrollHeight - window.innerHeight);
+//     });
+//   }
+// }
 
 function withAnyDo(value, callback){
   if(value instanceof Model){
-    value.listen(function(val){
+    value.listen(function(e){
+      var val = e.value;
       callback(val);
     });
   }
@@ -307,7 +233,8 @@ function withAnyDo(value, callback){
 
 function withNumberDo(value, callback){
   if(value instanceof Model){
-    value.listen(function(val){
+    value.listen(function(e){
+      var val = e.value;
       callback(Number(val));
     });
   }
@@ -323,7 +250,8 @@ function withStringDo(value, callback){
     return;
   }
   if(value instanceof Model){
-    value.listen(function(val){
+    value.listen(function(e){
+      var val = e.value;
       callback(String(val));
     });
   }
@@ -336,7 +264,8 @@ function withStringDo(value, callback){
 
 function withBooleanDo(value, callback){
   if(value instanceof Model){
-    value.listen(function(val){
+    value.listen(function(e){
+      var val = e.value;
       callback(Boolean(val));
     });
   }
